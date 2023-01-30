6898f4025c61b8d30d0bbb548f16f21af025b839
Martin-Melody <78037342+Martin-Melody@users.noreply.github.com>
2023-01-30 20:26:43 +0000

    simples fixes

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const { mongoose } = require("./db/mongoose");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 
 // Load in Mongoose models
